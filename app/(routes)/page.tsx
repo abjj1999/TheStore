c4ff7a2740adf3079b0d7b50c7f153d7fd20b3fd
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-09-25 16:32:55 -0500

    billboard displayed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("123c1a51-ee23-4683-b476-cddde1c1aaf6");
     return ( 
         <div className="">
-            <h1>Home Page</h1>
+            <Container>
+                <div className="space-y-10 pb-10">
+                    <Billboard data={billboard} />
+                </div>
+            </Container>
         </div>
      );
 }
