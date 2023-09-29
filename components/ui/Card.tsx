5a603846196ee61276a7f3a841fc6c66d139cd22
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-09-28 20:46:38 -0500

    single product list

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconBtn from "./IconBtn";
 import {  Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
 // import React from "react";
 
 interface ProductCardProps {
@@ -14,39 +15,43 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const router = useRouter();
+    const handleClick = () => {
+        router.push(`/products/${data?.id}`);
+    }
     return ( 
-        <div className="
-            bg-white group cursor-pointer rounded-xl border p-3 space-y-4
-        ">
-            <div className="aspect-square rounded-xl bg-gray-100 relative">
-                <Image alt="image" src={data?.images?.[0].url} fill
-                    className="aspect-square rounded-md object-cover"
-                />
-                <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
-                    <div className="flex gap-x-6 justify-center">
-                        <IconBtn 
-                            onClick={() => {}}
-                            icon={<Expand  size={20} className="text-gray-600"/>}
-                            className=""
-                        />
-                        <IconBtn 
-                            onClick={() => {}}
-                            icon={<ShoppingCart  size={20} className="text-gray-600"/>}
-                            className=""
-                        />
-                    </div>
-                </div>
-            </div>
-            {/* desc */}
-            <div className="">
-                <p className="font-semibold text-lg ">{data.name}</p>
-                <p className="text-sm text-gray-500">{data.category?.name}</p>
-            </div>
-            {/* price */}
-            <div className="flex items-center justify-between">
-                <Currency value={data.price} />
-            </div>
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* Image & actions */}
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image 
+          src={data.images?.[0]?.url} 
+          alt="" 
+          fill
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconBtn 
+              onClick={() => {}} 
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconBtn
+              onClick={() => {}} 
+              icon={<ShoppingCart size={20} className="text-gray-600" />} 
+            />
+          </div>
         </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price & Reiew */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
+      </div>
+    </div>
      );
 }
  
