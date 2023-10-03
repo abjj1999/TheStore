d8ff56dbeb32e48f1578c97debf23df12bd2ae19
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-10-03 15:51:16 -0500

    final stuff

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -10,16 +10,16 @@ const HomePage = async () => {
     const billboard = await getBillboard("123c1a51-ee23-4683-b476-cddde1c1aaf6");
     const products = await getProducts({isFeatured: true});
     return ( 
-        <div className="">
-            <Container>
-                <div className="space-y-10 pb-10">
-                    <Billboard data={billboard} />
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
-                </div>
-                </div>
-            </Container>
+        <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
         </div>
+      </div>
+    </Container>
      );
 }
  
